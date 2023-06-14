b19683a2fa3d4eafbe67ca3fc0a5493a07ba12df
Vladvldm <vlad474747@gmail.com>
2023-06-14 10:19:44 +0300

    Первая задача

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,11 +1,30 @@
 "use strict";
 
+/* --- Задача №1 --- */
+
 function solveEquation(a, b, c) {
   let arr = [];
+// Вычисляем дискримименант
+let discriminant = b ** 2 - 4 * a * c;
 
+if (discriminant < 0) {
+  return arr;
+} else if (discriminant === 0) {
+  let root = -b / (2 * a);
+  return arr.push(root);
+} else {
+  let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+  let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+  return arr.push(root1, root2);
+}
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {}
 
-// percent, contribution, amount, countMonths - процент, вклад, сумма, количество Месяцев
+/*
+percent - процентная ставка,
+contribution - вклад,
+amount - сумма вклада,
+countMonths - количество Месяцев
+*/
