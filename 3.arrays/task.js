6412cb1d69a9fbe7b6c11ed227b72cf1bbb4dc3f
Vladvldm <vlad474747@gmail.com>
2023-06-24 20:29:29 +0300

    Задачи 3-й блок готовы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  return arr1.every((val, index) => val === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const filteredUsers = users.filter((user) => user.gender === gender);
+  const totalAge = filteredUsers.reduce((acc, user) => acc + user.age, 0);
+  const avgAge = totalAge / (filteredUsers.length || 1);
+  return avgAge;
 }
