3a65b39a505a0178c84456bbddb7b6d49bbc6fe3
Vladvldm <vlad474747@gmail.com>
2023-07-02 23:36:10 +0300

    Задача декораторы код

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,50 @@
-//Задача № 1
-function cachingDecoratorNew(func) {
-  
-}
+"use strict";
 
-//Задача № 2
+/* --- Задача № 1 --- */
+
+function cachingDecoratorNew(func) {
+  let cache = [];
+
+  return (...args) => {
+    const hash = md5(args);
+    const objectInCache = cache.find((item) => item.hash === hash);
+
+    if (objectInCache) {
+      return "Из кэша: " + objectInCache.result;
+    }
+
+    const result = func(...args);
+    cache.push({ hash, result });
+
+    if (cache.length > 5) {
+      cache.shift();
+      console.log("Кеш переполнен, удаляем первый элемент");
+    }
+    return "Вычисляем: " + result;
+  };
+};
+
+/* --- Задача № 2 --- */
+
 function debounceDecoratorNew(func, delay) {
-  
-}
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  function wrapper(...args) {
+    if (timeoutId === null) {
+      func(...args);
+      wrapper.count++;
+    }
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      timeoutId = true;
+      func(...args);
+      wrapper.count++;
+    }, delay);
+    wrapper.allCount++;
+  }
+  return wrapper;
+};
